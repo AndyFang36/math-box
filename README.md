77333cdd0e23736f1a1b620e91a18054158c1cbd
AndyFang36 <andylovesapple666@icloud.com>
2022-03-08 19:23:44 +0800

    1. added method `sum` `average`; 2. fixed bugs; 3. improvements.

--- README.md
+++ README.md
@@ -4,17 +4,35 @@
 [![chat](https://img.shields.io/gitter/room/AndyFang36/math-magic)](https://img.shields.io/gitter/room/AndyFang36/math-magic)
 [![downloads](https://img.shields.io/npm/dw/math-magic)](https://img.shields.io/npm/dw/math-magic)
 
+<small>⚠tips: The new version fixed some fatal bugs. Thanks for using😋.</samll> 
 
 # math-magic
 Out-of-the-box math utilities.
 
-### Usage
-<code>
-        import {mathMagic} from "../index.js";
+--------------------------------------------------
 
-        console.log(mathMagic.random(3));
-        console.log(mathMagic.random(3, 4))
-        console.log(mathMagic.random(0, 2, 2));
-        console.log(mathMagic.factorial(5));
-        console.log(mathMagic.fibonacci(6)
-</code>
+### Getting Started  
+①. `npm install math-magic`  
+②. `import {mathMagic} from "math-magic"`
+
+### Usage
+1. random()  
+* `mathMagic.random()` => [0, 1)
+* `mathMagic.random(3)` => 0, 1, 2, 3
+* `mathMagic.random(3, 4)` => 3, 4
+* `mathMagic.random(0, 2, 2)` => [0.00, 2.00]
+
+2. factorial()  
+* `mathMagic.factorial(5)` => 120
+
+3. fibonacci()  
+* `mathMagic.fibonacci(6)` => [0, 1, 1, 2, 3, 5]
+
+4. sum()  
+* `mathMagic.sum([1, 2, 3, 4, 5,])` => 15
+
+5. average()
+* `mathMagic.average([1, 2, 3, 4, 5], 2)` => 3.50
+
+### License 
+* MPL-2.0 (Mozilla Public License, version 2.0)
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @description Out-of-the-box math utilities.
  * @author <a href="andylovesapple666@icloud.com">Andy Fang</a>
- * @version 1.0.0
+ * @version 1.1.0
  * @since 1.0.0
  * @license MPL-2.0
  */
@@ -29,7 +29,7 @@ export const mathMagic = {
                 while (result > max) {
                     result = (Math.random() * (max + 1 - min) + min).toFixed(decimalPlaces);
                 }
-                return Number.parseInt(result);
+                return Number.parseFloat(result);
             default:
                 return Math.random();
         }
@@ -52,17 +52,48 @@ export const mathMagic = {
     /**
      * @description This method returns the corresponding Fibonacci sequence.
      * @param {number} number The number of Fibonacci numbers.
-     * @return {number} An array of Fibonacci numbers.
+     * @return {number[]} An array of Fibonacci numbers.
      * @throws {Error} The entered number cannot be less than 0
      */
     fibonacci: function (number) {
-        let numbers = [0, 1, 1];
+        let numbers = [0, 1, 1,];
         if (number < 0) throw new Error("The entered number cannot be less than 0");
-        if (number >= 3) {
-            for (let i = 3; i <= number; i++) {
+        if (number > 3) {
+            for (let i = 3; i < number; i++) {
                 numbers[i] = numbers[i - 1] + numbers[i - 2];
             }
         }
         return numbers;
-    }
+    },
+
+    /**
+     * @description This method calculates the sum of a set of numbers.
+     * @param {number[]} numbers The array to be summed.
+     * @return {number} The sum.
+     */
+    sum: function (numbers) {
+        let sum = 0;
+        numbers.forEach(number => {
+            sum += number;
+        });
+        return sum;
+    },
+
+    /**
+     * @description This method calculates the average of a set of numbers.
+     * @param {number[]} numbers The array to be averaged.
+     * @param {number} decimalPlaces The number of decimal places to retain.
+     * @return {string} The average value.
+     * */
+    average: function (numbers, decimalPlaces = 0) {
+        let result = this.sum(numbers) / numbers.length;
+        switch (arguments.length) {
+            case 1:
+                return result.toString();
+            case 2:
+                return result.toFixed(decimalPlaces);
+            default:
+                throw new Error("Only 1 or 2 parameters can be entered!");
+        }
+    },
 }
